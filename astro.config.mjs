d38f371d016debee78467254e94151a06aa32281
Jochum van der Ploeg <jochum@cardinal.run>
2025-01-07 11:20:16 +0100

    refactor: rename to Cardinal

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://cardinality.run',
+    site: 'https://cardinal.run',
 });
